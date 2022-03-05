3d3544eb2726bf472f3e4de0e2248f0ce6ba6821
brenda <lamasbrenda@hotmail.com>
2022-03-05 15:34:12 -0300

    agregue colores y botones

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const juguetes = document.querySelector("#juguetes")
 const ropa = document.querySelector("#ropa")
 const envios = document.querySelector("#envios")
 
+const pampers = document.querySelector("#pampers")
+const huggies = document.querySelector("#huggies")
+const babysec = document.querySelector("#babysec")
+const estrella = document.querySelector("#estrella")
+const duffy = document.querySelector("#duffy")
+const caricias = document.querySelector("#caricias")
+
+
+const cardPampers = document.querySelector("#card-paniales-pampers")
+const cardHuggies = document.querySelector("#card-paniales-huggies")
+const cardBabysec = document.querySelector("#card-paniales-babysec")
+const cardEstrella = document.querySelector("#card-paniales-estrella")
+const cardDuffy = document.querySelector("#card-paniales-duffy")
+const cardCaricias = document.querySelector("#card-paniales-caricias")
 
 
 
@@ -21,6 +35,13 @@ paniales.onclick =()=>{
     sectionPaniales.style.display="flex"
     sectionEnvios.style.display="none"
     sectionInicio.style.display="none"
+
+    cardPampers.style.display="none"
+    cardHuggies.style.display="none"
+    cardBabysec.style.display="none"
+    cardEstrella.style.display="none"
+    cardDuffy.style.display="none"
+    cardCaricias.style.display="none"
 }
 inicio.onclick =()=>{
     sectionPaniales.style.display="none"
@@ -49,4 +70,53 @@ envios.onclick =()=>{
     sectionJuguetes.style.display="none"
     sectionRopa.style.display="none"
     sectionEnvios.style.display="flex"
+}
+// click en los botones de seccion paniales
+pampers.onclick = () =>{
+    cardPampers.style.display="flex"
+    cardHuggies.style.display="none"
+    cardBabysec.style.display="none"
+    cardEstrella.style.display="none"
+    cardDuffy.style.display="none"
+    cardCaricias.style.display="none"
+}
+huggies.onclick = () =>{
+    cardPampers.style.display="none"
+    cardHuggies.style.display="flex"
+    cardBabysec.style.display="none"
+    cardEstrella.style.display="none"
+    cardDuffy.style.display="none"
+    cardCaricias.style.display="none"
+}
+estrella.onclick = () =>{
+    cardPampers.style.display="none"
+    cardHuggies.style.display="none"
+    cardBabysec.style.display="none"
+    cardEstrella.style.display="flex"
+    cardDuffy.style.display="none"
+    cardCaricias.style.display="none"
+}
+babysec.onclick = () =>{
+    cardPampers.style.display="none"
+    cardHuggies.style.display="none"
+    cardBabysec.style.display="flex"
+    cardEstrella.style.display="none"
+    cardDuffy.style.display="none"
+    cardCaricias.style.display="none"
+}
+duffy.onclick = () =>{
+    cardPampers.style.display="none"
+    cardHuggies.style.display="none"
+    cardBabysec.style.display="none"
+    cardEstrella.style.display="none"
+    cardDuffy.style.display="flex"
+    cardCaricias.style.display="none"
+}
+caricias.onclick = () =>{
+    cardPampers.style.display="none"
+    cardHuggies.style.display="none"
+    cardBabysec.style.display="none"
+    cardEstrella.style.display="none"
+    cardDuffy.style.display="none"
+    cardCaricias.style.display="flex"
 }
